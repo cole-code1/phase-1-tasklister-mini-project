1da81361dfca21aed2fe5980b697c9d770c0ecb8
Collins Kathurima <collinskathu313@gmail.com>
2024-10-06 22:15:59 +0300

    complete challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   // your code here
+document.querySelector("#create-task-form").addEventListener("submit",(event) => {
+  event.preventDefault();
+  const des=document.getElementById("new-task-description").value;
+const tasks=document.getElementById("tasks");
+tasks.innerHTML += `<li> ${des} 
+<button id="remove">x</button></li>`;
+const remove = document.getElementById("remove")
+remove.addEventListener("click", (et) => {
+  et.target.parentNode.remove()
+})
+
+
+
+
+
 });
+ 
